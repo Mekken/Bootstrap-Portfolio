ffa7b1fe1bb3f17a292d495a10ad477effc83404
Donnie Heard <dhearddev@gmail.com>
2018-06-28 22:34:25 -0700

    added new project

---
+++ assets/js/portfolio.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+    function openNewTab(url)
+    {
+        window.open(url,'_blank');
+    }
+
+    $('#train-schedule').on('click',function() {
+        openNewTab('https://mekken.github.io/train-schedule/');
+    }).hover(function() {
+        $(this).toggleClass('shadow-sm p-3 mb-5 bg-white rounded');
+    })
+
+
+})
